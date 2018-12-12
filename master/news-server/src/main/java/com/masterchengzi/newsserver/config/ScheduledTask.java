b51dd8cbac103b3aadfa675713dbfcc3bf23b7d1
zjc <1173727598@qq.com>
2018-12-12 14:47:35 +0800

    add

--- master/news-server/src/main/java/com/masterchengzi/newsserver/config/ScheduledTask.java
+++ master/news-server/src/main/java/com/masterchengzi/newsserver/config/ScheduledTask.java
@@ -2,20 +2,13 @@ package com.masterchengzi.newsserver.config;
 
 import com.masterchengzi.mastercommon.common.JsonResult;
 import com.masterchengzi.mastercommon.common.myDate;
-import com.masterchengzi.newsgetserver.server.GetNews;
-import com.masterchengzi.newsgetserver.server.GetNewsFeign;
-import com.masterchengzi.newsserver.entity.GetNewsWithBLOBs;
 import com.masterchengzi.newsserver.service.GetNewsService;
 import lombok.extern.log4j.Log4j2;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 @Component
 @Log4j2
@@ -26,8 +19,8 @@ public class ScheduledTask {
 
 	@Scheduled(cron = "0 0 0 * * ?")  // 每天凌晨
 	public void getJuheNewsTop() {
-		JsonResult result= getNewsService.delete(null,null,null,null,new Date(118,0,1),myDate.getdate(new Date(),-2));
-		log.info(result.getMessage()+"删除两天前的数据:"+result.getData());
+		JsonResult result = getNewsService.delete(null, null, null, null, new Date(118, 0, 1), myDate.getdate(new Date(), -2));
+		log.info(result.getMessage() + "删除两天前的数据:" + result.getData());
 	}
 
 }
