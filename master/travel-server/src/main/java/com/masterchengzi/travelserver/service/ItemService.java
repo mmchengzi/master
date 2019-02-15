b3a6e22fe9aeeab30c616bb043cba26e4d9d4792
zjc <1173727598@qq.com>
2019-02-15 17:36:41 +0800

    add

--- master/travel-server/src/main/java/com/masterchengzi/travelserver/service/ItemService.java
+++ master/travel-server/src/main/java/com/masterchengzi/travelserver/service/ItemService.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface ItemService {
     JsonResult getList(Integer itemId,String itemName, String type, Date beginTime, Date endTime);
-    JsonResult getPage(Integer itemId,String itemName, String type, Date beginTime, Date endTime, Integer pageNum, Integer pageSize);
+    JsonResult getPage(Integer itemId, String itemName, String type, Date beginTime, Date endTime, Integer pageNum, Integer pageSize);
     JsonResult delete(Integer itemId);
     JsonResult insert(List<Item> record);
     JsonResult update(List<Item> record);
