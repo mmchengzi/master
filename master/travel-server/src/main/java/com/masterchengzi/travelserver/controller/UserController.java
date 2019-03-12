fadbf6b442107deab0f117b243a528f20660eff9
zjc <1173727598@qq.com>
2019-03-12 23:52:42 +0800

    添加微信登陆

--- master/travel-server/src/main/java/com/masterchengzi/travelserver/controller/UserController.java
+++ master/travel-server/src/main/java/com/masterchengzi/travelserver/controller/UserController.java
@@ -26,24 +26,26 @@ public class UserController {
     @GetMapping("/getList")
     public JsonResult getList(@RequestParam(value = "userId", required = false) Integer userId,
                               @RequestParam(value = "username", required = false) String username,
+                              @RequestParam(value = "openid", required = false) String openid,
                               @RequestParam(value = "email", required = false) String email,
                               @RequestParam(value = "phone", required = false) String phone,
                               @RequestParam(value = "beginTime", required = false) Date beginTime,
                               @RequestParam(value = "endTime", required = false) Date endTime) {
-        return service.getList(userId, username, email, phone, beginTime, endTime);
+        return service.getList(userId, username, openid,email, phone, beginTime, endTime);
     }
 
     @ApiOperation(value = "分页查询用户")
     @GetMapping("/getPage")
     public JsonResult getPageNews(@RequestParam(value = "userId", required = false) Integer userId,
                                   @RequestParam(value = "username", required = false) String username,
+                                  @RequestParam(value = "openid", required = false) String openid,
                                   @RequestParam(value = "email", required = false) String email,
                                   @RequestParam(value = "phone", required = false) String phone,
                                   @RequestParam(value = "beginTime", required = false) Date beginTime,
                                   @RequestParam(value = "endTime", required = false) Date endTime,
                                   @RequestParam(name = "pageNum", required = false) int pageNum,
                                   @RequestParam(name = "pageSize", required = false) int pageSize) {
-        return service.getPage(userId, username, email, phone, beginTime, endTime, pageNum, pageSize);
+        return service.getPage(userId, username,openid, email, phone, beginTime, endTime, pageNum, pageSize);
     }
 
     @ApiOperation(value = "删除用户")
